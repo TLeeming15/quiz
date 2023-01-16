fe1f23606b7bbb3ae778a483ddd115506b42fa32
TLeeming15 <tyler.leeming21@gmail.com>
2023-01-17 06:56:51 +1030

    universal selectors

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,12 @@
 // Universal variables
+var scores =[];
+var mark = 0;
+var index = 0;
+var record = [];
+var timeLeft = 70;
+var timerInterval 
+var header = document.querySelector(".header");
+var home = document.querySelector(".home");
+var container = document.querySelector(".container");
+var bottom = document.querySelector(".bottom");
+var outcome = document.querySelector(".outcome");
